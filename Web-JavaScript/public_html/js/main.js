1db7f92a20ba63e1a005ed3ef7ba386df3f5d729
Animation <Animation@192.168.0.67>
2014-01-19 20:19:02 +0400

    made the dynamic clock to webpage with setInterval method

--- Web-JavaScript/public_html/js/main.js
+++ Web-JavaScript/public_html/js/main.js
@@ -1,2 +1,9 @@
+function loadTikTik(){
+    //setTimeout(myTime1());
+    setInterval('myTime1()',1000); 
+}
 
-
+function myTime1() {
+    var myDate = new Date();
+    document.getElementById("clock").innerHTML = myDate.getHours() + " : " + myDate.getMinutes() + " : " + myDate.getSeconds();
+}
